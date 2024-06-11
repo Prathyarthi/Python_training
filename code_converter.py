b54cb14322e3191a5312b20e1a03b76fa25fa774
Prathyarthi <prathyarti@gmail.com>
2024-06-11 12:28:39 +0530

    Added languages to convert

--- code_converter.py
+++ code_converter.py
@@ -11,7 +11,26 @@ model = genai.GenerativeModel('gemini-1.5-flash')
 code = '''n = 5
 for i in range(n):
     print(" "*(n-i) + "* "*(i+1))'''
-language = "javascript"
+
+language = ""
+
+print("1. Javascript \n2. Java \n3. C++ \n4. C \n5. Python")
+choice = input("Enter choice: ")
+if choice=="1":
+    language = 'javascript'
+
+if choice=="2":
+    language = 'Java'
+
+if choice=="3":
+    language = 'C++'
+
+if choice=="4":
+    language = 'C'
+
+if choice=="5":
+    language = 'Python'
+
 prompt = f"Convert this code {code} to {language}"
 
 response = model.generate_content(prompt)
